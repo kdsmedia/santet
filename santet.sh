6eb4e87a8c4c813b2a98eaaa401811bd7bed2a64
SIDHANIE <137620314+kdsmedia@users.noreply.github.com>
2024-05-08 05:24:43 +0700

    Update and rename infect.sh to santet.sh

---
+++ santet.sh
@@ -0,0 +1,308 @@
+#DILARANG RECODE BOSS
+
+#ALTOMEDIA
+
+#-----------------
+
+red='\033[1;31m'
+
+rset='\033[0m'
+
+grn='\033[1;32m'
+
+ylo='\033[1;33m'
+
+blue='\033[1;34m'
+
+cyan='\033[1;36m'
+
+pink='\033[1;35m'
+
+#-----------------
+
+# scripting starts
+
+clear
+
+echo '
+
+                
+
+                 __________
+
+               .~#########%%;~.
+
+              /############%%;`\
+
+             /######/~\/~\%%;,;,\
+
+            |#######\    /;;;;.,.|
+
+            |#########\/%;;;;;.,.|
+
+ XX         |##/~~\####%;;;/~~\;,|       XX
+
+ XX         |#|  o  \##%;/  o  |.|       XX
+
+ XX         |##\____/##%;\____/.,|       XX
+
+ XX         \#########/\;;;;;;,, /       XX
+
+ XX           \######/%;\;;;;, /         XX
+
+ XX            |######%%;;;;,.|          XX  
+
+ XX            ----------------          XX 
+
+           XX   Santet v1.1    XX        
+          
+             XX              XX          
+
+               XX          XX            
+
+                 XX      XX             
+
+                   XX  XX             
+
+                     ALTOMEDIA
+'|lolcat
+
+echo " " 
+
+echo -e "$red     ▶ Coded by$grn ALTOMEDIA$red ◀$rset"
+
+echo -e "$red     ⫸$ylo @altomediacloudbot$red ⫷$rset"
+
+echo -e "$red    ▶$grn INDONESIA CYBER SYSTEM$red ◀$rset"
+
+echo 
+
+# Functions
+
+echo -e "$red       [Menyantet Dengan Perasaan]$rset"
+
+echo " "
+
+echo " "
+
+echo -e "$grn      ＞>>>＞$ylo [Options]$grn ＜<<<＜$rset"
+
+echo " "
+
+echo " "
+
+echo -e "$red      ➡$cyan [1️⃣] Santet Sekarang"
+
+echo -e "$red      ➡$cyan [2️⃣] Simpan Pengaturan"
+
+echo -e "$red     ➡️$cyan [3️⃣] Update alat santet"
+
+echo -e "$red      ➡$cyan [4️⃣] Keluar"
+
+echo " "
+
+echo " "
+
+echo -e "$grn   ＞＞＞＞$ylo [SELECT]$grn ＜＜＜<$rset"
+
+echo " "
+
+echo -e "$cyan   [[[$ylo Select any option$cyan]]]$rset"
+
+echo " "
+
+read n
+
+case "$n" in
+
+1)echo " "
+
+echo " "
+
+echo " "
+
+echo -e "$red  Kamu akan$grn Santet$red Now$rset"
+
+echo " "
+
+echo -e "$grn  Press$ylo ENTER$red to continue$rset"
+
+echo " "
+
+read hulke
+
+cd $HOME/santet/lol
+
+echo " "
+
+echo -e "$ylo  Untuk mengakhiri proses klik$red 't'$ylo atau untuk melanjutkan klik$grn ENTER$rset" 
+
+read choice
+
+if [ $choice = 't' ] ; then
+
+echo -e "$red      Apakah kamu yakin tekan$grn ENTER$red to exit$rset"
+
+read hulkee
+
+cd $HOME/santet
+
+bash santet.sh
+
+else
+
+bash sysup.sh
+
+echo " "
+
+echo " "
+
+echo -e "$grn ＞＞＞＞＞＞$ylo [EXIT]$grn ＜＜＜＜＜＜$rset"
+
+echo " "
+
+echo -e "$cyan  click$grn ENTER$cyan untuk kembali ke menu Santet$rset"
+
+echo " "
+
+read punch
+
+fi
+
+cd $HOME/santet
+
+bash santet.sh ;;
+
+2)echo " "
+
+echo " "
+
+echo " "
+
+echo -e "$red     kamu akan$red Seve Korban$rset "
+
+echo " "
+
+echo -e "$grn     Press$ylo ENTER$grn untuk melanjutkan$rset"
+
+echo " "
+
+read nitro
+cd $HOME/santet/lol
+
+echo " "
+
+echo -e "$ylo   Untuk mengakhiri proses klik$red 't' $ylo atau untuk melanjutkan klik$grn ENTER$rset"
+
+read choice
+
+if [ $choice = 't' ] ; then
+
+echo -e "$red       Apakah kamu yakin tekan$grn ENTER$red untuk keluar$rset"
+
+read nitroo
+cd $HOME/santet/
+
+bash santet.sh
+
+else
+
+bash save.sh
+
+echo " "
+
+echo " "
+
+echo -e "$grn ＞＞＞＞＞$ylo [EXIT]$grn ＜＜＜＜＜＜＜$rset"
+
+echo " "
+
+echo -e "$cyan    click$grn ENTER$cyan untuk kembali ke menu Santet$rset"
+
+echo " "
+
+read punch
+
+fi
+cd $HOME/santet
+bash santet.sh ;;
+
+3)echo " "
+
+echo " "
+
+echo " "
+
+echo -e "$red     Kamu akan$grn Perbarui$red Script$rset "
+
+echo " "
+
+echo -e "$grn     Press$ylo ENTER$grn to continue$rset"
+
+echo " "
+
+read mon
+cd $HOME/santet/lol
+
+echo " "
+
+echo -e "$ylo         Untuk mengakhiri proses klik$red 't' $ylo atau untuk melanjutkan klik$grn ENTER$rset"
+
+read choice
+
+if [ $choice = 't' ] ; then
+
+echo -e "$red                 Apakah kamu yakin tekan$grn ENTER$red atau exit$rset"
+
+read monn
+cd $HOME/santet
+
+bash santet.sh
+
+else
+
+bash update.sh
+
+echo " "
+
+echo " "
+
+echo -e "$grn ＞＞＞＞＞＞＞＞$ylo [EXIT]$grn ＜＜＜＜＜＜＜＜＜$rset"
+
+echo " "
+
+echo -e "$cyan        click$grn ENTER$cyan kembali ke menu santet$rset"
+
+echo " "
+
+read punch
+
+fi
+cd $HOME/santet
+
+bash santet.sh ;;
+
+4)
+
+sleep 2.0
+
+clear
+
+echo " "
+
+echo -e "$grn
+
+     ____  _  _  ____    ____  _  _  ___
+     
+    (  _ \( \/ )( ___)  (  _ \( \/ )( ___)
+
+     ) _ < \  /  )__)    ) _ < \  /  )__)
+
+    (____/ (__) (____)  (____/ (__) (____)...$cyan semoga dosamu tidak menurun ke anak cucu$rset"
+
+echo " "
+
+echo " "
+
+esac
+
